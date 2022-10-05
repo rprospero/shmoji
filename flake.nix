36955ff84cc8c4b7a64a584a05004b9b7e6c4c17
Adam Washington <adam.washington@stfc.ac.uk>
2022-10-05 16:21:48 +0100

    fully package flake

--- flake.nix
+++ flake.nix
@@ -3,13 +3,22 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-utils.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
-        packages.default =
-          nixpkgs.legacyPackages.x85_64-linux.stdenv.mkDerivation {
-            name = "shmoji";
-          };
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "shmoji";
+          src = ./.;
+          buildInputs = [ pkgs.wtype pkgs.rofi ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp shmoji $out/bin
+            wrapProgram $out/bin/shmoji --prefix PATH : ${
+              pkgs.lib.makeBinPath [ pkgs.wtype pkgs.rofi ]
+            }
+          '';
+        };
       });
 }
